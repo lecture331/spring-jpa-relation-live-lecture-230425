517dcbf842df4fedd8f46c0ec2801c4c01fd46d8
Robbie <lecture@>
2023-04-25 19:18:50 +0900

    init commit

---
+++ src/main/java/com/example/springjparelationlivelecture230425/SpringJpaRelationLiveLecture230425Application.java
@@ -0,0 +1,13 @@
+package com.example.springjparelationlivelecture230425;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaRelationLiveLecture230425Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaRelationLiveLecture230425Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springjparelationlivelecture230425/SpringJpaRelationLiveLecture230425ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjparelationlivelecture230425;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaRelationLiveLecture230425ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
